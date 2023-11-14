8de64404f9ba71999d46d69436b7fbc8657fa9f3
Diogo Costa <diogocosta876@hotmail.com>
2023-11-14 18:04:04 +0000

    deleted main (now run Client and MiddleManServer instead)

--- java-maven-project/src/main/java/org/example/Client.java
+++ java-maven-project/src/main/java/org/example/Client.java
@@ -183,6 +183,9 @@ public class Client {
     }
 
 
-
-
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.attemptHandshake();
+        client.run();
+    }
 }
