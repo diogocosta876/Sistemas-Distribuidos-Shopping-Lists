2037629ce280a299efb8992f40c4e3ad3319d25e
Miguel Teixeira <themutedguy@gmail.com>
2023-11-22 11:19:47 +0000

    first commmit CRDT changes

---
+++ src/main/java/org/example/ShoppingList/CRDTItem.java
@@ -0,0 +1,45 @@
+package org.example.ShoppingList;
+
+import java.io.Serializable;
+
+public class CRDTItem implements Serializable {
+
+
+    private String itemName;
+    private int quantity;
+    private String userId;
+    private long timestamp;
+
+    public CRDTItem(String itemName) {
+        this.itemName = itemName;
+        this.quantity = 0;
+        this.userId = "";
+        this.timestamp = 0L;
+    }
+
+    // Update the item with new quantity, user ID, and timestamp
+    public void updateItem(int quantity, String userId, long timestamp) {
+        if (timestamp > this.timestamp || (timestamp == this.timestamp && userId.compareTo(this.userId) > 0)) {
+            this.quantity = quantity;
+            this.userId = userId;
+            this.timestamp = timestamp;
+        }
+    }
+
+    public void merge(CRDTItem otherItem) {
+        if (otherItem.timestamp > this.timestamp || (otherItem.timestamp == this.timestamp && otherItem.userId.compareTo(this.userId) > 0)) {
+            this.quantity = otherItem.quantity;
+            this.userId = otherItem.userId;
+            this.timestamp = otherItem.timestamp;
+        }
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    // Get the user ID of the last updater
+    public String getUserId() {
+        return userId;
+    }
+}
--- src/main/java/org/example/ShoppingList/ShoppingList.java
+++ src/main/java/org/example/ShoppingList/ShoppingList.java
@@ -1,70 +1,40 @@
 package org.example.ShoppingList;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.io.Serializable;
+import java.util.Map;
+import java.util.HashMap;
 
-public class ShoppingList {
-    private String name;
-    private String userId;
-    private UUID uuid;
-    private List<Item> items;
+public class ShoppingList implements Serializable {
 
-    public ShoppingList(String name) {
-        this.name = name;
-        this.items = new ArrayList<>();
-        this.uuid = java.util.UUID.randomUUID();
+
+    private final String listId;
+    private final Map<String, CRDTItem> items;
+
+    public ShoppingList(String listId) {
+        this.listId = listId;
+        this.items = new HashMap<>();
     }
 
-    public ShoppingList(){
-        this.items = new ArrayList<>();
+    // Add or update an item in the shopping list
+    public void addItem(String itemName, int quantity, String userId, long timestamp) {
+        CRDTItem item = items.computeIfAbsent(itemName, k -> new CRDTItem(itemName));
+        item.updateItem(quantity, userId, timestamp);
     }
 
-    public void addItem(Item item) {
-        items.add(item);
+    // Get the current state of the shopping list
+    public Map<String, CRDTItem> getState() {
+        return new HashMap<>(items);
     }
 
-    public void removeItem(int index) {
-        if (index >= 0 && index < items.size()) {
-            items.remove(index);
+    // Get the ID of the shopping list
+    public String getListId() {
+        return listId;
+    }
+
+    // Merge two shopping lists
+    public void merge(ShoppingList otherList) {
+        for (Map.Entry<String, CRDTItem> entry : otherList.items.entrySet()) {
+            items.computeIfAbsent(entry.getKey(), k -> new CRDTItem(entry.getKey())).merge(entry.getValue());
         }
-    }
-
-    // Getters and setters
-
-    public String getName() {
-        return this.name;
-    }
-    public UUID getUUID() {
-        return uuid;
-    }
-
-    public void setName(String name){
-        this.name = name;
-    }
-
-    public List<Item> getItems(){
-        return this.items;
-    }
-
-
-
-
-    public void displayList() {
-            List<Item> items = this.items;
-            System.out.println("Items:");
-            for (int i = 0; i < items.size(); i++) {
-                Item item = items.get(i);
-                System.out.println((i + 1) + ". " + item.getName() + " (Quantity: " + item.getQuantity() + ")");
-
-            }
-            System.out.print("\n");
-    }
-    public void saveToFile() {
-        // Implement saving the shopping list to a file
-    }
-
-    public void loadFromFile() {
-        // Implement loading the shopping list from a file
     }
 }
